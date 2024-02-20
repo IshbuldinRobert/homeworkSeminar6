75f605173b1630cebd8071565d83bab9e820a146
Robert <ishbuldinrobert2002@gmail.com>
2024-02-20 16:26:38 +0500

    hw

---
+++ Main.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class Main{
+    public static void main(String[] args){
+        Persister persister = new Persister(new User("Bob"));
+
+        persister.report();
+        persister.save();
+    }
+}
